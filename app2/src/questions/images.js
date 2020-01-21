3016f1dadcb9e25ee06fced167ed8cf59bcd8d4a
Erik Larko <erik.larko@wattpad.com>
2020-01-20 21:08:48 -0500

    Clarify ImageThatNeedsToBeLoaded

--- app2/src/questions/images.js
+++ app2/src/questions/images.js
@@ -6,8 +6,8 @@
 export type PreloadedImage = number;
 
 // Represents an image that will likely need a loading indicator.
-// The reason for the strange `typeof foo` is because the official flow type
-// cannot be imported and includes many properties that I don't want to
+// The reason for the strange `typeof exampleImage` is because the official flow
+// type cannot be imported and includes many properties that I don't want to
 // redefine here.
-export type ImageThatNeedsToBeLoaded = typeof foo;
-const foo = { uri: './path/to/some/image.png' };
+const exampleImage = { uri: './path/to/some/image.png' };
+export type ImageThatNeedsToBeLoaded = typeof exampleImage;
