46e3653e03e0508ac529e157c16d5cebf54b239b
Erik Larko <erik.larko@wattpad.com>
2020-04-19 15:01:04 -0400

    Update navigation docs; fix EmotionDetails: null error

--- app2/src/navigation/index.js
+++ app2/src/navigation/index.js
@@ -1,4 +1,18 @@
 // @flow
+//
+// Screens are defined in react-navigation/screens.js
+//
+// Navigation usage:
+//   import * as React from 'react';
+//   import { Buttom } from 'react-native';
+//   import { navigator } from '../navigation';
+//
+//   export function ButtonThatNavigates(props: {}) {
+//     return <Button
+//              title="navigate to home"
+//              onPress={ () => navigator.resetToHome() }
+//            />
+//   }
 
 export { navigator } from './react-navigation/navigator.js';
 export { mockReactNavigation as installMockNavigator } from './react-navigation/mock.js';
--- app2/src/navigation/react-navigation/screens.js
+++ app2/src/navigation/react-navigation/screens.js
@@ -14,6 +14,6 @@ export const screens = {
         screen: Home,
     },
     EmotionDetails: {
-        screen: null,
+        screen: Home,
     },
 };
