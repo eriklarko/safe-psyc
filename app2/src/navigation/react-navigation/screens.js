25b2ecb4ff3649c6b889fffcff4e2fc452f0a553
Erik Larkö <erik.larko@wattpad.com>
2020-11-21 17:08:13 -0500

    Remove AnimationTest

--- app2/src/navigation/react-navigation/screens.js
+++ app2/src/navigation/react-navigation/screens.js
@@ -4,15 +4,11 @@ import { Home } from '../../home';
 import { Loading } from '../../loading';
 import { Session } from '../../session';
 import { Report } from '../../report';
-import { AnimationTest } from '../../styles/animation-test';
 
 export type Screen = $Keys<typeof screens>;
 
 export const initialScreen: Screen = 'Loading';
 export const screens = {
-    AnimationTest: {
-        screen: AnimationTest,
-    },
     Loading: {
         screen: Loading,
     },
