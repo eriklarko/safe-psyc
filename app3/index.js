c3559717c0628d15dc70a37ad5bb2e7daca87f6f
Erik <erik@larko.se>
2022-10-04 14:37:55 -0400

    WIP: New typescript app

---
+++ app3/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+
+import { RootComponent } from './src/navigation/react-navigation/root-component.js';
+
+AppRegistry.registerComponent(appName, () => RootComponent);
